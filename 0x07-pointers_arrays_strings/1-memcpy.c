af1f5071b84914343c70df272512fe2891f8e7a1
Your Name <you@example.com>
2023-08-01 06:08:08 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * memcpy- a function that copy memory
+ * @dest:memory storage
+ * @src:copied memory
+ * @n: number of bytes
+ *
+ * return: copied memory with n bytes changed
+ */
+
+char*_memcpy(char*dest, char*src, unsigned int n)
+{
+	int r=o;
+	int i=n
+
+for (; r<i, r++)
+{
+dest[r]=src[r];
+n--;
+}
+return (dest);
+}
